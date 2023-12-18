d568bbe64470078b1339714dec988baa5ffe212a
Wijnand Karsens <wijnand@karsens.com>
2023-12-18 20:50:05 +0100

    improvements

--- src/components/getMdHtml.ts
+++ src/components/getMdHtml.ts
@@ -1,18 +1,26 @@
 import { Marpit } from "@marp-team/marpit";
 
-export const getMdHtml = (markdown:string)=>{
-      // 1. Create instance (with options if you want)
-  const marpit = new Marpit({inlineSVG:true});
+export const getMdHtml = (markdown: string) => {
+  // 1. Create instance (with options if you want)
+  const marpit = new Marpit({ inlineSVG: true });
 
   // 2. Add theme CSS
   const theme = `
-/* @theme example */
+/* @theme default */
 
 section {
   background-color: #369;
   color: #fff;
   font-size: 30px;
   padding: 40px;
+}
+
+header,
+footer {
+  position: absolute;
+  left: 50px;
+  right: 50px;
+  height: 20px;
 }
 
 h1,
@@ -27,10 +35,8 @@ h1 {
 `;
   marpit.themeSet.default = marpit.themeSet.add(theme);
 
-  
   // 3. Render markdown
 
-  
   const { html, css } = marpit.render(markdown);
 
   // 4. Use output in your HTML
@@ -43,5 +49,5 @@ h1 {
   ${html}
 </body></html>
 `;
-return htmlFile;
-}
+  return htmlFile;
+};
